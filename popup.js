ad661f395d3ab5670e4f9b80f43c9b657bb05451
Erika McVey <Erika McVey>
2016-01-23 16:50:35 -0500

    more keywords, mocked fill-in

--- popup.js
+++ popup.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
         console.log(tab);
         console.log(tab.url);
         document.getElementById('keyword-1').innerHTML = 'dogs';
+        document.getElementById('keyword-2').innerHTML = 'cats';
+        document.getElementById('keyword-3').innerHTML = 'mice';
+        document.getElementById('keyword-4').innerHTML = 'horsies';
+        document.getElementById('keyword-5').innerHTML = 'cows';
+        document.getElementById('art-length').innerHTML = '15';
+        document.getElementById('sent-score').innerHTML = '25';
     });
 }
 );
