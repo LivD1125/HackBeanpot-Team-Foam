5c921174b27a15239b91acb386702e28202cec81
Erika McVey <Erika McVey>
2016-01-23 11:03:09 -0500

    commiting popup js for liv

--- popup.js
+++ popup.js
@@ -1,29 +1,9 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-function request(file) {
-    var filename = file;
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            xhttp.responseType = 'json';
-            xhttp.onload = function () {
-                // Parse and process the response from the
-                var response = xhttp.response;
-                if (!response || !response.responseData || !response.responseData.results ||
-                    response.responseData.results.length === 0) {
-                    errorCallback('No response from the Server!');
-                    return;
-                }
-            }
-        }
-        ;
-        xhttp.open("GET", filename, true);
-        xhttp.send();
-    };
-}
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('anything?');
-    var element = document.getElementById("keyword-1");
-    element.innerHTML = "Maybe this will work!";
-});
+var URL = window.location.hostname + window.location.pathname;
+console.log(URL);
+
+$.post(
+    'https://apiv2.indico.io/keywords?key=80d27da1ef84a0e43229a30692747ada&version=2',
+    JSON.stringify({
+        'data': "Some call the the the sunshine it the sunshine state"
+    })
+).then(function(res) { console.log(res) });
