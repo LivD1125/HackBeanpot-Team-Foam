7909132987afb1d6333ff26be5c5b9887df7d175
Erika McVey <Erika McVey>
2016-01-23 15:57:35 -0500

    delete commented lines

--- popup.js
+++ popup.js
@@ -1,13 +1,3 @@
-//console.log($('h1'));
-//chrome.runtime.sendMessage({action: 'get_info'}, function(res) {
-//    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//        if( request.message === 'add_information') {
-//            console.log('hmmmmmmm');
-//            console.log(request.object);
-//        }
-//    });
-//});
-//
 $(document).ready(function(){
     chrome.tabs.getSelected(null, function(tab){
         console.log(tab);
@@ -16,13 +6,3 @@ $(document).ready(function(){
     });
 }
 );
-
-
-
-
-//chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//    if( request.message === 'add_information') {
-//        console.log('hmmmmmmm');
-//        console.log(request.object);
-//    }
-//});
