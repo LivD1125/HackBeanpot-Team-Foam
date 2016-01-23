af3fc9ed85f21c6e8eb84b2af09eae903e7f0858
sharma-abhi <sharma.abhi@husky.neu.edu>
2016-01-23 10:02:33 -0500

    added the JS POST functionality

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 /* background.js */
 chrome.browserAction.onClicked.addListener(function(tab) {
+	alert('inside background.js')
     chrome.tabs.executeScript(tab.id, {file: "link.js"});
 });
--- flask/flaskr.py
+++ flask/flaskr.py
@@ -3,6 +3,7 @@ import sqlite3, sys, subprocess
 from flask import Flask, request, session, g, redirect, url_for, \
 	 abort, render_template, flash
 from contextlib import closing
+from flask.ext.cors import CORS
 
 # configuration
 DEBUG = True
@@ -11,6 +12,7 @@ SECRET_KEY = 'development key'
 
 # create our little application :)
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(__name__)
 #app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
@@ -19,10 +21,11 @@ def index(name=None):
 	#return 'Hello World!'
 	if request.method == 'POST':
 		url = request.form.get('url')
-		flash('Successfully downloaded!', 'success')
-		#flash(url)
+		print url;
+		#flash('Successfully downloaded!', 'success')
+		flash(url)
 		#return redirect(url_for('download'))
-		return render_template('show_entries.html', name=name)
+		return render_template('show_entries.html', name=url)
 	else:
 		return render_template('show_entries.html', name=name)
 
