310fa2f306235899d1b8850b34302d2b2fd42c5d
sharma-abhi <sharma.abhi@husky.neu.edu>
2016-01-23 09:16:29 -0500

    added flask basic

---
+++ flask/README.md
@@ -0,0 +1,2 @@
+# flaskr
+Flask Tutorial at http://flask.pocoo.org/docs/0.10/tutorial/
---
+++ flask/flaskr.py
@@ -0,0 +1,31 @@
+# all the imports
+import sqlite3, sys, subprocess
+from flask import Flask, request, session, g, redirect, url_for, \
+	 abort, render_template, flash
+from contextlib import closing
+
+# configuration
+DEBUG = True
+SECRET_KEY = 'development key'
+
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+#app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+@app.route('/', methods=['GET', 'POST'])		
+def index(name=None):
+	#return 'Hello World!'
+	if request.method == 'POST':
+		url = request.form.get('url')
+		flash('Successfully downloaded!', 'success')
+		#flash(url)
+		#return redirect(url_for('download'))
+		return render_template('show_entries.html', name=name)
+	else:
+		return render_template('show_entries.html', name=name)
+
+if __name__ == '__main__':
+	#app.run(host='0.0.0.0')
+	app.run(host = '0.0.0.0', port=5000, debug=True)
