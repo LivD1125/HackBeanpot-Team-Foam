49a53c89c51db81907ac0db164d0ad4b7ccfc9a0
Olivia DeFlumeri <Olivia DeFlumeri>
2016-01-23 11:22:25 -0500

    Adding more maybe API Calls from JS

--- popup.js
+++ popup.js
@@ -7,3 +7,18 @@ $.post(
         'data': "Some call the the the sunshine it the sunshine state"
     })
 ).then(function(res) { console.log(res) });
+
+
+$.post(
+     'https://apiv2.indico.io/sentiment?key=ac77d1c220266970c98f8f6ef9aaa5fe',
+    JSON.stringify({
+                       'data': "Some call the the the sunshine it the sunshine state"
+                   })
+).then(function(res) { console.log(res) });
+
+$.post(
+    'https://apiv2.indico.io/namedentities?key=ac77d1c220266970c98f8f6ef9aaa5fe',
+    JSON.stringify({
+                       'data': "Some call the the the sunshine it the sunshine state"
+                   })
+).then(function(res) { console.log(res) });
