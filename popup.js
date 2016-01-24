2b1f054d0cfe065233c211fc2ab6dfdb99853a01
NadineShaalan <NadineShaalan@Nadines-MacBook-Pro.local>
2016-01-23 23:50:45 -0500

    Style Changes and Java script

--- popup.js
+++ popup.js
@@ -7,12 +7,38 @@ $(document).ready(function(){
 			    'http://0.0.0.0:5000/',
 			    {'url': tab.url}
 			    ).then(function(result){
-			    	//console.log(result);
-			    	document.getElementById('keyword-1').innerHTML = result;
-			    	//document.getElementById('keyword-1').innerHTML = 'dogs';
-			    	alert(result);
+			    	console.log(result);
+			    	var minutes = 10;
+			    	var attitude = .83;
+			    	var topics = ["Bernie Sanders", "Hillary Clinton", "Democratic", "Election"];
+			    	var percentageEl = document.getElementById('percentage');
+			        var imageEl = document.getElementById('picture');
+
+			    	document.getElementById('time').innerHTML = minutes;
+
+			    	if (attitude > .5){
+			    		percentageEl.className= "positive";
+			    		imageEl.src = "pos.png"
+			    	}
+			    	else {
+			    		percentageEl.className= "negative";
+						imageEl.src = "neg.png"
+			    	}
+
+			    	percentageEl.innerHTML= Math.round(attitude * 100) + "%";
+			    	var topicsList = "";
+			    	for (var x=0; x < Math.min(5, topics.length); x++){
+			    		if (x!=0){
+			    		 topicsList += "-"
+			    		}
+			    		topicsList += topics[x];
+			    	}
+			    	document.getElementById('keywords').innerHTML = topicsList;
+			    	//alert(result);
 			    });
+
 
     });
 }
 );
+
